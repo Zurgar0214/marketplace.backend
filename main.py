d9c03dbc84613531356d823f98b58ad11247ca61
Juan Diego Ramirez Muñoz <jramiezm@icetex.gov.co>
2024-05-18 11:52:00 -0500

    create db

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI, status, APIRouter, Depends
 from sqlalchemy.orm import Session
 from dddpy.domain.Entities.post import Post
-from dddpy.insfrastructure.sqlite.database import get_db
+from dddpy.insfrastructure.sqlite.database import get_db, Base, engine
 from dddpy.insfrastructure.sqlite.repository.repository import GenericRepository
 from dddpy.insfrastructure.sqlite.schemas.post_dto import PostDTO
 
 app = FastAPI()
-router = APIRouter()
+Base.metadata.create_all(bind= engine)
 
 
 @app.get(
@@ -18,10 +18,10 @@ async def healthCheck():
     return {"message": "All works!"}
 
 
-@app.get("/posts/{post_id}")
-def get_post(post_id: str, db: Session = Depends(get_db)):
+@app.post("/posts")
+async def get_post(post_id: str, db: Session = Depends(get_db)):
     repository = PostRepository(db)
-    post = repository.get(post_id)
+    post = repository.post()
     return post.to_dto()
 
 class PostRepository(GenericRepository[Post]):
