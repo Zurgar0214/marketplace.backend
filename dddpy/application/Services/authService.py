191f2770c1db404203e4f39d4b3b8e3c104412fc
Juan Diego Varon Valencia <juan.varonv@opitech.com.co>
2024-06-06 11:16:49 -0500

    fix in get_user_by_name_and_role method

--- dddpy/application/Services/authService.py
+++ dddpy/application/Services/authService.py
@@ -1,11 +1,12 @@
 from fastapi import Depends
+from sqlalchemy.orm import Session
 from dddpy.application.Models.loginDataModel import UserLogin
 from dddpy.application.Models.userModel import UserModel
 from dddpy.domain.schemas.user_dto import UserDTO
 from dddpy.insfrastructure.Auth.jwt_manage import encode_jwt
 from dddpy.insfrastructure.services.encryptService import check_password
 from dddpy.insfrastructure.sqlite.database import get_db
-from sqlalchemy.orm import Session
+
 
 def validate_password(db_user: UserDTO, user:UserLogin):
     if check_password(user.password, db_user.password):
@@ -21,5 +22,5 @@ def get_token(data: dict):
 
 
 def get_user_by_username_and_role(data: dict, db: Session = Depends(get_db)):
-    user = db.query(UserModel).filter(UserModel.email == data["email"]).first()
+    user = db.query(UserDTO).filter(UserDTO.email == data["email"]).first()
     return user
--- dddpy/insfrastructure/Auth/jwt_depends.py
+++ dddpy/insfrastructure/Auth/jwt_depends.py
@@ -1,20 +1,22 @@
-from fastapi import HTTPException, Request
+from fastapi import Depends, HTTPException, Request
 from fastapi.security import HTTPBearer
+from sqlalchemy.orm import Session
 
 from dddpy.application.Services.authService import get_user_by_username_and_role
 from dddpy.insfrastructure.Auth.jwt_manage import decode_jwt
+from dddpy.insfrastructure.sqlite.database import get_db
 
 
 class JWTBearer(HTTPBearer):
 
-    async def __call__(self, request: Request):
+    async def __call__(self, request: Request, db: Session = Depends(get_db)):
         credentials = await super().__call__(request)
         
         data = decode_jwt(credentials.credentials)
         
         if data:
             #TODO: Cambiar esto a un servicio.
-            user = get_user_by_username_and_role(data)
+            user = get_user_by_username_and_role(data, db)
             
             if user:
                 return user
